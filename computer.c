52dc6e20a5806b70462476ec8d721354ebcfef8d
Nicolas Germeau <nicolas.germeau@gmail.com>
2021-06-04 21:46:44 +0200

    load operations

--- computer.c
+++ computer.c
@@ -7,11 +7,13 @@ typedef __uint8_t byte;
 struct Memory  
 {
     byte mar;
-    byte ram[16][16];
+    byte ram[256];
 };
 
 struct CPU {
+  byte regs[4];
   byte iar;
+  byte ir;
   byte bus1;
   byte tmp;
   byte acc;
@@ -35,30 +37,22 @@ void alu(byte opcode){
 }
 
 void execute_step1(){
-  
-  // enable    
-  // put bus1 to 1 and put IAR on the bus
-  computer.cpu.bus1 = 1; 
+  computer.cpu.bus1 = 1; // enable: put bus1 to 1 and put IAR on the bus
   computer.bus = computer.cpu.iar;
-  
-  // set 
-  // ALU compute new IAR (Bus input + bus 1) and put it into ACC
-  // put bus (IAR value) into MAR 
-  alu(0);
+  alu(0); // set ALU compute new IAR (Bus input + bus 1) and put it into ACC and put bus 
   computer.memory.mar = computer.bus;
 }
 
-
-
 void execute_step2(){
-  printf("step 2, put instruction from memory into IR \n");     
-  /* computer.bus.value = fetchInstruction(computer.memory.memoryAddressRegister) */
-  /* computer.cpu.instructionRegister = computer.bus.value; */
+  computer.bus = computer.memory.ram[computer.bus]; //enable: put ram content at addr in bus on bus
+  computer.cpu.ir = computer.bus; //set: put bus into instruction register
 }
 
 void execute_step3(){
-  printf("step 3, put content of ACC to IAR\n");     
+  computer.bus = computer.cpu.acc; //enable:: put acc in bus
+  computer.cpu.iar = computer.bus; //set: put bus into iar
 }
+
 
 // the 6 steps taken as a whole is called a instruction cycle 
 // first 3 steps are fetch 
@@ -72,22 +66,66 @@ void do_step(){
     }
     if (currentStep == 3){
       execute_step3();
+    }
+    if (currentStep == 4){
       currentStep = 0;
     }
 
     currentStep+=1;
 }
 
+// from ram to register, first operand contains the address
+void load(operand1,operand2){
+  //step 4
+  computer.bus = computer.cpu.regs[operand1]; //enable
+  computer.memory.mar = computer.bus; //set
+  //step 5 
+  computer.bus = computer.memory.ram[computer.memory.mar]; //enable
+  computer.cpu.regs[operand2] = computer.bus; //set
+}
+
+// from register to ram, first operand contains the address
+void store(operand1,operand2){
+  //step 4 
+  computer.bus = computer.cpu.regs[operand1]; //enable
+  computer.memory.mar = computer.bus; //set
+  //step 5 
+  computer.bus = computer.cpu.regs[operand2]; //enable
+  computer.cpu.regs[operand2] = computer.bus; //set
+}
+
+
+void execute_instruction(){
+  byte operation = computer.cpu.ir >> 4; 
+  byte operand1 = (computer.cpu.ir & 12) >> 2;
+  byte operand2 = (computer.cpu.ir & 3);
+  
+  if (operation == 0){
+    load(operand1,operand2);
+  }else if (operation = 1){
+    store();
+  }
+  
+}
+
 void cycle(){
   for (int i = 0; i < 10; i++){
     // each step is one cycle 
     do_step();
-    printf("iar %i", computer.cpu.iar);
+    printf("iar %i\n", computer.cpu.iar);
   }
 }
 
 int main(){
-cycle();
+  enum day{mon,tue,wed}today;
+  today = mon;          
+  printf("%i /n",today);
+  
+  
+  
+
+
+//cycle();
 }
 
 
@@ -98,12 +136,8 @@ cycle();
 
 
 
-//1 read bin file 
-//2 create new computer 
-//3 load program into memory
-//5 run computer 
-  //5.1 put addressRegister to beginning of memory 
-  //tick cpu (one tick up and one tick down)
+
+
 
 
 
--- readme.md
+++ readme.md
@@ -1,43 +1,8 @@
 # Todo
-[ ] ajouter l'utilisation du bus 
-[ ] clock e,s, en fonction de leur valeur on execute la partie d'une step (ex step 1enable/set)
-
-overview
-#
-
-1) load the program into memory 
-2) run the computer (clock start to create cycle)
 
 
-clock    
-generate cycle:    
-  1 cycle = down and up   
-  for every cycle call next step   (one step per cycle)
+Planning
+----
+- pouvoir executer un programme qui fait une somme simple 
+- pouvoir parler avec un clavier et un ecran 
 
-stepper    
-  move from step to step    
-  step 1     
-  step 2    
-  step 3     
-      we didn't execute anything, just loaded the next instruction from ram into the registry and increment by 1 the index
-  step 4    
-  step 5    
-  step 6    
-  step 7    
-
-cpu:
-  registers 
-  acc
-  tmp 
-  IR 
-  index 
-
-memory: 
-  mar 
-  ram 
-   
-bus: 
-  value  
-  
-  
-  find the link between a 1 in an instruction for defining a register and the register on the graph 
